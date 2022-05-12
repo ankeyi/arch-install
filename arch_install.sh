7a587961281a595124ad7ff824f1e794a47b00c9
ankeyi <86785926+ankeyi@users.noreply.github.com>
2022-05-12 21:52:07 +0800

    user

--- arch_install.sh
+++ arch_install.sh
@@ -41,6 +41,3 @@ chown open_source:open_source /home/open_source/enable.sh
 # settings > start up  enable script
 systemctl enable NetworkManager
 systemctl enable sddm
-pacman -S  grub efibootmgr
-grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
